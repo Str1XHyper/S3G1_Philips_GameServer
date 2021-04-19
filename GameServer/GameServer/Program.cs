c2c00e42c03b62d120f17452b5afcba61a96b5f4
Str1XHyper <tijnvanveghel@gmail.com>
2021-04-19 13:12:18 +0200

    Setup server

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace GameServer
 {
+    public class Mercier : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var msg = e.Data == "Test"
+                      ? "I've been balused already..."
+                      : "I'm not available now.";
+
+            Send(msg);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var wssv = new WebSocketServer("ws://localhost:4000");
+            wssv.AddWebSocketService<Mercier>("/Mercier");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
         }
     }
 }
