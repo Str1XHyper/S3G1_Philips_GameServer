3dacd749553b3529a843693fb90623e9f060da33
Luc Somers <lucsomers@hotmail.com>
2021-04-19 14:25:58 +0200

    Fixed connection issue

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -6,13 +6,36 @@ namespace GameServer
 {
     public class Mercier : WebSocketBehavior
     {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("Open socket");
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            base.OnError(e);
+
+            Console.WriteLine("eigen message van de OnError");
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.Exception);
+        }
+
         protected override void OnMessage(MessageEventArgs e)
         {
             var msg = e.Data == "Test"
                       ? "I've been balused already..."
                       : "I'm not available now.";
 
+            Console.WriteLine("eigen message van de OnMessage");
+
             Send(msg);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+
+            Console.WriteLine("eigen message van de onclose");
         }
     }
     class Program
